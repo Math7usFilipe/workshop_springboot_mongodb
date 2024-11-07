e80a6bd02ace3817889b509be69dfbb0b5c83a9c
Math7usFilipe <mxtheus829melo@gmail.com>
2024-11-07 15:52:32 -0300

    Connecting to MongoDB with repository and service

--- src/test/java/com/curso/workshopmongo/domain/User.java
+++ src/test/java/com/curso/workshopmongo/domain/User.java
@@ -3,7 +3,9 @@ package com.curso.workshopmongo.domain;
 import java.io.Serializable;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
--- src/test/java/com/curso/workshopmongo/resources/UserResource.java
+++ src/test/java/com/curso/workshopmongo/resources/UserResource.java
@@ -1,27 +1,30 @@
 package com.curso.workshopmongo.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.workshopmongo.domain.User;
+import com.curso.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")  // O caminho do endpoint
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping  // Utilizando o @GetMapping, mais específico para requisições GET
     public ResponseEntity<List<User>> findAll() {
         // Criando usuários manualmente
-        User maria = new User("1", "Maria Silva", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        
 
         // Colocando os usuários em uma lista
-        List<User> list = Arrays.asList(maria, alex);
+        List<User> list = service.findAll();
 
         // Retornando a lista de usuários com o código HTTP 200 (OK)
         return ResponseEntity.ok().body(list);
