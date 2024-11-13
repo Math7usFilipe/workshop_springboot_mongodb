582ea91bc4839ba7fe439c9f4ec10efb6cff7836
Math7usFilipe <mxtheus829melo@gmail.com>
2024-11-13 13:14:25 -0300

    Referencing user posts

--- src/test/java/com/curso/workshopmongo/config/Instantiation.java
+++ src/test/java/com/curso/workshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.curso.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,5 +46,8 @@ public class Instantiation implements CommandLineRunner {
 
         postRepository.save(post1);
         postRepository.save(post2);
+
+        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        userRepository.save(maria);
     }
 }
